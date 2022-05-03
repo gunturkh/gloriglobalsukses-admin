2d4269a97941b5590e2467a5b8c7443cb0bef23b
Guntur KH <guntur.kh@gmail.com>
2022-05-03 12:36:50 +0700

    feat: implement socket for qr and logout button

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,28 +1,63 @@
 import React, { useEffect, useState } from "react";
 import { Card, CardContent, CardHeader } from "@mui/material";
-import { Loading, useNotify } from "react-admin";
+import { Button, inferTypeFromValues, Loading, useNotify } from "react-admin";
 import QRCode from "react-qr-code";
+import socketIOClient from "socket.io-client";
 
 const url = process.env.REACT_APP_SERVER_URL;
+const SOCKET_ENDPOINT = "http://127.0.0.1:4001";
 export const Dashboard = () => {
   const notify = useNotify();
 
-  const [qr, setQr] = useState({ message: "", data: null });
+  const [socketData, setSocketData] = useState({ message: "", data: "" });
+  const [socketClientData, setSocketClientData] = useState({});
+  const [qr, setQr] = useState({ message: "", data: "" });
+  const [clientInfo, setClientInfo] = useState({});
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const intervalId = setInterval(() => {
-      handleGetQR();
-    }, 5000);
+    const socket = socketIOClient(SOCKET_ENDPOINT);
+    socket.on("FromAPI", (data) => {
+      console.log("socket data", data);
+      console.log(" typeof socket data", typeof data);
+      setLoading(false);
+      setSocketData(data);
+    });
+    socket.on("ClientInfo", (data) => {
+      setSocketClientData(data);
+    });
 
     return () => {
-      clearInterval(intervalId);
+      socket.disconnect();
     };
   }, []);
 
+  // useEffect(() => {
+  //   const intervalId = setInterval(() => {
+  //     handleGetQR();
+  //   }, 5000);
+
+  //   return () => {
+  //     clearInterval(intervalId);
+  //   };
+  // }, []);
+
   useEffect(() => {
-    if (qr?.message === "authenticated") notify("Whatsapp authenticated");
-  }, [notify, qr]);
+    // if (qr?.message === "authenticated") notify("Whatsapp authenticated");
+    setQr(socketData);
+    console.log("socketData qr", socketData);
+  }, [socketData]);
+
+  useEffect(() => {
+    setClientInfo(socketClientData);
+    console.log("ClientInfo", clientInfo);
+    // const info = clientInfo;
+    // info &&
+    //   console.log(`*Connection info*
+    // User name: ${info.pushname}
+    // My number: ${info.wid.user}
+    // Platform: ${info.platform}`);
+  }, [socketClientData]);
 
   const handleGetQR = async () => {
     setLoading(true);
@@ -34,14 +69,34 @@ export const Dashboard = () => {
     }
   };
 
-  console.log("qr data", qr);
+  const handleWALogout = async () => {
+    setLoading(true);
+    const response = await fetch(`${url}/auth/wa-logout`);
+    const result = await response.json();
+    if (result) {
+      console.log("WA Logout Success", result);
+    }
+  };
+
+  // console.log("qr data", qr);
   return (
     <Card>
       <CardHeader title="Welcome to Tracking Management System Dashboard" />
       <CardContent>
-        {qr?.message !== "authenticated"
-          ? "Please scan QR code below"
-          : "WA Authentication Success!"}
+        <div>
+          Status:{" "}
+          {qr?.message !== "authenticated"
+            ? "Please scan QR code below"
+            : "WA Authentication Success!"}
+        </div>
+        {qr?.message === "authenticated" && (
+          <>
+            <div>Username: {socketClientData?.pushname ?? ""}</div>
+            <div>Phone number: {socketClientData?.wid?.user ?? ""}</div>
+            <div>Platform: {socketClientData?.platform ?? ""}</div>
+            <button onClick={handleWALogout}> Logout </button>
+          </>
+        )}
       </CardContent>
       {loading ? (
         <Loading loadingPrimary="QR Code Loading..." />
@@ -50,7 +105,7 @@ export const Dashboard = () => {
           {qr && qr?.message !== "authenticated" && (
             <>
               <div style={{ background: "white", padding: "16px" }}>
-                <QRCode value={qr?.data?.qr} />
+                <QRCode value={qr?.data} />
               </div>
             </>
           )}
