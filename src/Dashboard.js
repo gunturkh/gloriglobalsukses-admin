5a3723a40327915eec54ffc5478afe8d127c15c3
Guntur KH <guntur.kh@gmail.com>
2022-05-04 09:56:52 +0700

    feat: socket address and persist qr and client info

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -5,26 +5,30 @@ import QRCode from "react-qr-code";
 import socketIOClient from "socket.io-client";
 
 const url = process.env.REACT_APP_SERVER_URL;
-const SOCKET_ENDPOINT = "http://127.0.0.1:4001";
+const SOCKET_ENDPOINT = process.env.REACT_APP_SOCKET_ENDPOINT || "http://127.0.0.1:4001";
 export const Dashboard = () => {
   const notify = useNotify();
 
+  const parsedClientInfoFromSessionStorage = JSON.parse(sessionStorage.getItem('clientInfo')) || {}
+  const parsedQRFromSessionStorage = JSON.parse(sessionStorage.getItem('qr')) || {}
+  const checkSavedClientInfo = Object.keys(parsedClientInfoFromSessionStorage).length > 0
+  const checkSavedQR = Object.keys(parsedQRFromSessionStorage).length > 0
   const [socketData, setSocketData] = useState({ message: "", data: "" });
-  const [socketClientData, setSocketClientData] = useState({});
+  const [socketClientData, setSocketClientData] = useState(JSON.parse(sessionStorage.getItem('clientInfo')));
   const [qr, setQr] = useState({ message: "", data: "" });
-  const [clientInfo, setClientInfo] = useState({});
-  const [loading, setLoading] = useState(true);
+  const [clientInfo, setClientInfo] = useState(parsedClientInfoFromSessionStorage);
+  const [loading, setLoading] = useState(checkSavedQR ? false : true);
 
   useEffect(() => {
     const socket = socketIOClient(SOCKET_ENDPOINT);
     socket.on("FromAPI", (data) => {
-      console.log("socket data", data);
-      console.log(" typeof socket data", typeof data);
       setLoading(false);
       setSocketData(data);
+      sessionStorage.setItem('qr', JSON.stringify(data))
     });
     socket.on("ClientInfo", (data) => {
       setSocketClientData(data);
+      sessionStorage.setItem('clientInfo', JSON.stringify(data))
     });
 
     return () => {
@@ -43,20 +47,11 @@ export const Dashboard = () => {
   // }, []);
 
   useEffect(() => {
-    // if (qr?.message === "authenticated") notify("Whatsapp authenticated");
     setQr(socketData);
-    console.log("socketData qr", socketData);
   }, [socketData]);
 
   useEffect(() => {
     setClientInfo(socketClientData);
-    console.log("ClientInfo", clientInfo);
-    // const info = clientInfo;
-    // info &&
-    //   console.log(`*Connection info*
-    // User name: ${info.pushname}
-    // My number: ${info.wid.user}
-    // Platform: ${info.platform}`);
   }, [socketClientData]);
 
   const handleGetQR = async () => {
@@ -74,26 +69,29 @@ export const Dashboard = () => {
     const response = await fetch(`${url}/auth/wa-logout`);
     const result = await response.json();
     if (result) {
-      console.log("WA Logout Success", result);
+      await sessionStorage.removeItem('clientInfo')
+      await sessionStorage.removeItem('qr')
+      setSocketClientData({})
+      setClientInfo({})
     }
   };
 
-  // console.log("qr data", qr);
   return (
-    <Card>
+    <Card sx={{ height: '100%' }}>
       <CardHeader title="Welcome to Tracking Management System Dashboard" />
       <CardContent>
-        <div>
+        <div style={{ marginBottom: 6 }}>
           Status:{" "}
-          {qr?.message !== "authenticated"
-            ? "Please scan QR code below"
-            : "WA Authentication Success!"}
+          {checkSavedClientInfo
+            ? "WA Authentication Success!"
+            : "Please scan QR code below"
+          }
         </div>
-        {qr?.message === "authenticated" && (
+        {checkSavedClientInfo && (
           <>
-            <div>Username: {socketClientData?.pushname ?? ""}</div>
-            <div>Phone number: {socketClientData?.wid?.user ?? ""}</div>
-            <div>Platform: {socketClientData?.platform ?? ""}</div>
+            <div style={{ marginBottom: 6 }}>Username: {clientInfo?.pushname ?? ""}</div>
+            <div style={{ marginBottom: 6 }}>Phone number: {clientInfo?.wid?.user ?? ""}</div>
+            <div style={{ marginBottom: 6 }}>Platform: {clientInfo?.platform ?? ""}</div>
             <button onClick={handleWALogout}> Logout </button>
           </>
         )}
@@ -102,10 +100,10 @@ export const Dashboard = () => {
         <Loading loadingPrimary="QR Code Loading..." />
       ) : (
         <>
-          {qr && qr?.message !== "authenticated" && (
+          {checkSavedQR && !checkSavedClientInfo && (
             <>
               <div style={{ background: "white", padding: "16px" }}>
-                <QRCode value={qr?.data} />
+                <QRCode value={parsedQRFromSessionStorage?.data} />
               </div>
             </>
           )}
