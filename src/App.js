29c73a5b82f1c58d799b680c6eed913e63201b23
Guntur KH <guntur.kh@gmail.com>
2022-05-09 06:46:11 +0700

    fix: remove unused function

--- src/App.js
+++ src/App.js
@@ -31,20 +31,6 @@ const fetchJson = (url, options = {}) => {
 
 const dataProvider = simpleRestProvider(apiUrl, fetchJson);
 
-/**
- * Convert a `File` object returned by the upload input into a base 64 string.
- * That's not the most optimized way to store images in production, but it's
- * enough to illustrate the idea of data provider decoration.
- */
-const convertFileToBase64 = (file) =>
-  new Promise((resolve, reject) => {
-    const reader = new FileReader();
-    reader.onload = () => resolve(reader.result);
-    reader.onerror = reject;
-
-    reader.readAsDataURL(file.rawFile);
-  });
-
 const customDataProvider = {
   ...dataProvider,
   getList: async (resource, params) => {
